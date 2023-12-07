b0b22c11ade6d65bf8151cd5d22853fb47993e19
Michael Ofagbor <eihmenormike@gmail.com>
2023-12-07 03:05:54 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: HTMLSlotElement(
+          
+        )
+      }
+    },
+  },
+  plugins: [],
+}
+
